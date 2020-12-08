97097759e9d2930d3d408cea0babcfdaa7e00795
miseriae <zuyev.frm@gmail.com>
2020-12-08 16:57:25 +0200

    Add authentication

--- account/views.py
+++ account/views.py
@@ -1,7 +1,11 @@
+import jwt
+
 from django.shortcuts import render
 from rest_framework.generics import GenericAPIView
 from rest_framework.response import Response
 from rest_framework import status
+from django.conf import settings
+from django.contrib import auth
 
 from .serializers import UserSerializer
 
@@ -15,3 +19,25 @@ class RegisterView(GenericAPIView):
             serializer.save()
             return Response(serializer.data)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class LoginView(GenericAPIView):
+    serializer_class = UserSerializer
+
+    def post(self, request):
+        data = request.data
+        username = data.get('username', '')
+        password = data.get('password', '')
+        user = auth.authenticate(username=username, password=password)
+
+        if user:
+            auth_token = jwt.encode({'username': user.username}, settings.JWT_SECRET_KEY)
+            serializer = UserSerializer(user)  # many=True
+            data = {
+                'user': serializer.data,
+                'token': auth_token
+            }
+            
+            return Response(data, status=status.HTTP_200_OK)
+
+        return Response({'detail': 'Invalid credentials'}, status=status.HTTP_401_UNAUTHORIZED)
